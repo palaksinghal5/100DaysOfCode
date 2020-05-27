2b5ed47b083125ee049adf15c309c01b566b9c64
Palak Singhal <49813217+palaksinghal5@users.noreply.github.com>
2020-05-28 01:32:16 +0530

    Create Arrays Introduction.cpp

---
+++ Arrays Introduction.cpp
@@ -0,0 +1,16 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int n,arr[1000];
+    cin>>n;
+    for(int i=0;i<n;i++){cin>>arr[i];}
+    for(int i=n-1;i>=0;i--){cout<<arr[i]; cout<<" ";} 
+    cout<<endl;
+    return 0;
+}
