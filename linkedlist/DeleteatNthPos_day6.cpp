6f05fa0c2e38d487189f7b2d2f920d5565e06185
Palak Singhal <49813217+palaksinghal5@users.noreply.github.com>
2020-05-31 17:35:22 +0530

    Create DeleteatNthPos_day6.cpp

---
+++ linkedlist/DeleteatNthPos_day6.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+using namespace std;
+struct node{
+    int data;
+    node* link;
+};
+node* head;
+void Insertatend(int x){
+    node* temp=new node();
+    temp->data=x;
+    temp->link=NULL;
+    node* temp1=head;
+    if(temp1==NULL){
+        head=temp;
+    }
+    else{
+        while(temp1->link!=NULL){
+            temp1=temp1->link;
+        }
+        temp1->link=temp;
+    }
+    
+}
+void Print(){
+    node* temp=head;
+    while(temp!=NULL){
+        cout<<temp->data<<" ";
+        temp=temp->link;
+    }
+    cout<<endl;
+}
+void DeleteatN(int x){
+    if(x==1){
+        node* temp=head;
+        head=temp->link;
+        free(temp);
+        return;
+    }
+        
+    
+    node* temp =head;
+    for(int i= 0;i<x-2;i++){
+        temp=temp->link;
+    }
+    node* temp1=temp->link;
+    temp->link=temp1->link;
+    free(temp1);
+}
+int main(){
+    head= NULL;
+    Insertatend(2);
+    Insertatend(3);
+    Insertatend(4);
+    Insertatend(5);
+    DeleteatN(2);
+    Print();
+    
+}
