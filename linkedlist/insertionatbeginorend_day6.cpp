cc748ce2b4e1dd2b9c65eeb82e8275eaa77f2144
Palak Singhal <49813217+palaksinghal5@users.noreply.github.com>
2020-05-31 15:53:10 +0530

    Create insertionatbeginorend_day6.cpp
    Linked List implementation
    1. Adding data when list is empty
    2. Adding data at the end of the list

---
+++ linkedlist/insertionatbeginorend_day6.cpp
@@ -0,0 +1,61 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include<iostream>
+using namespace std;
+struct node{
+    int data;
+    node* link;
+};
+node* head;
+
+void InsertatEnd(int x){
+  node* temp=new node();
+  temp->data=x;
+  temp->link=NULL;
+  node* temp1=head;
+  
+  if(temp1==NULL)
+  {
+      head=temp;
+  }
+  else
+  {
+  
+    while(temp1->link!=NULL)
+    {
+      temp1=temp1->link;
+      
+    }
+    temp1->link=temp;
+  } 
+}
+
+void Print(){
+    node* temp=head;
+    cout<<"list is ";
+    while(temp!=NULL)
+    {
+      cout<<temp->data<<" ";
+      temp=temp->link;
+    }
+    cout<<endl;
+}
+int main(){
+    head=NULL;
+    int n;
+    cout<<"enter total numbers to be added in list:\n";
+    cin>>n;
+    for(int i=0;i<n;i++){
+        int a;
+        cin>>a;
+        InsertatEnd(a);
+         
+    }Print();
+    
+}
