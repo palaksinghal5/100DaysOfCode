fbd521617aba07e5ecfb16504d62f89348264c46
Palak Singhal <49813217+palaksinghal5@users.noreply.github.com>
2020-06-11 23:18:17 +0530

    Create Day_17_GenerateBinaryNumber.cpp

---
+++ Queue/Day_17_GenerateBinaryNumber.cpp
@@ -0,0 +1,32 @@
+
+
+#include <iostream>
+#include<queue>
+
+using namespace std;
+
+void GenerateBinary(int n){
+    queue<string> q;
+    q.push("1");
+    
+    while(n--){
+        string s1=q.front();
+        q.pop();
+        cout<<s1<<"\n";
+        
+        string s2=s1;
+        
+        q.push(s1.append("0"));
+        q.push(s2.append("1"));
+        
+    }
+    
+    
+}
+
+int main()
+{
+    int n=10;
+    GenerateBinary(n);
+    return 0;
+}
