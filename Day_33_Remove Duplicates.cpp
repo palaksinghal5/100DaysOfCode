a390c889208f545f8d965594d834bab2173f796e
Palak Singhal <49813217+palaksinghal5@users.noreply.github.com>
2020-06-29 01:08:33 +0530

    Update Day_33_Remove Duplicates.cpp

--- Day_33_Remove Duplicates.cpp
+++ Day_33_Remove Duplicates.cpp
@@ -1,34 +1,61 @@
-#include<iostream> 
-using namespace std; 
 
-int removeDup(int arr[], int n) 
-{ 
-    if (n==0 || n==1) 
-        return n; 
-  
-    int j = 0; 
-  
 
-    for (int i=0; i < n-1; i++) 
-        if (arr[i] != arr[i+1]) 
-            arr[j++] = arr[i]; 
-  
-    arr[j++] = arr[n-1]; 
-  
-    return j; 
-} 
-  
- 
-int main() 
-{ 
-    int arr[] = {1, 2, 2, 3, 4, 4, 4, 5, 5}; 
-    int n = sizeof(arr)/sizeof(arr[0]); 
-  
+#include <iostream>
 
-    n = removeDup(arr,n); 
-  
-    for (int i=0; i<n; i++) 
-        cout << arr[i] << " "; 
-  
-    return 0; 
-} 
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        
+        
+    int p;
+    cin>>p;
+    int a[p];
+    int x=0;
+    int arr[p];
+    for(int j=0;j<p;j++)
+    {
+        cin>>a[j];
+    }
+    
+    for(int j=0;j<p-1;j++){
+        if(a[j]!=a[j+1]){
+            arr[x]=a[j];
+            x++;
+        }
+    }
+    if(a[p-1]!=arr[x-1]){
+        arr[x]=a[p-1];
+        x++;
+    }
+    
+    for(int j=0;j<x;j++){
+        cout<<arr[j]<<" ";
+    }
+    
+    cout<<"\n";
+    }
+
+    return 0;
+}
+
+
+/******************************************************************************
+Input--> 
+
+2
+5
+2 2 2 2 2 
+3
+1 2 2
+
+Output-->
+
+2 
+1 2 
+
+*******************************************************************************/
