3b45c052570af42e6c73ea8b604e2b09fcc6dd8e
Palak Singhal <49813217+palaksinghal5@users.noreply.github.com>
2020-09-12 17:31:27 +0530

    Create Day_63_LongestIncreasingSubsequence.cpp

---
+++ Dynamic Programming/Day_63_LongestIncreasingSubsequence.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int lcs(int arr[],int n){
+    int lis[n];
+    lis[0]=1;
+    
+    for(int i=1;i<n;i++)
+{
+        lis[i]=1;
+        
+    for(int j=0;j<i;j++)
+    {
+        if(arr[i]>arr[j]&&lis[i]<lis[j]+1)
+        {
+            lis[i]=lis[j]+1;
+        }
+    }    
+}
+
+return *max_element(lis,lis+n);
+}
+
+int main(){
+    
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    
+    cout<<lcs(a,n);
+    
+    return 0;
+}
